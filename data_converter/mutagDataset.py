d09386c6e0b3091aca566612401c670f69e54972
ahadjawaid <ahadjawaid0@gmail.com>
2022-07-01 13:36:56 -0500

    added model

--- data_converter/mutagDataset.py
+++ data_converter/mutagDataset.py
@@ -2,6 +2,7 @@ from dgl.data import DGLDataset
 import dgl
 import torch
 from processData import processData
+from processDataOneNtype import processDataOneNtype
 
 class MUTAG(DGLDataset):
     def __init__(self):
@@ -10,6 +11,24 @@ class MUTAG(DGLDataset):
     def process(self):
         getGraphData, _ = processData()
         graphData = getGraphData()
+        self.graphs = []
+        for graphID in graphData.keys():
+            graph, label = graphData[graphID]
+            self.graphs.append([dgl.heterograph(graph), label])
+           
+    def __getitem__(self, i):
+        return self.graphs[i]
+
+    def __len__(self):
+        return len(self.graphs)
+
+
+class MUTAGOneNtype(DGLDataset):
+    def __init__(self):
+        super().__init__(name='mutag')
+        
+    def process(self):
+        graphData = processDataOneNtype()
         self.graphs = []
         for graphID in graphData.keys():
             graph, label = graphData[graphID]
