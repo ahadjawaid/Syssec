fd356aabdcbaf9bda128794e84ff8d57a63fb2dd
ahadjawaid <ahadjawaid0@gmail.com>
2022-07-01 19:21:54 -0500

    removed onentype version

--- data_converter/mutagDataset.py
+++ data_converter/mutagDataset.py
@@ -2,7 +2,6 @@ from dgl.data import DGLDataset
 import dgl
 import torch
 from processData import processData
-from processDataOneNtype import processDataOneNtype
 
 class MUTAG(DGLDataset):
     def __init__(self):
@@ -11,24 +10,6 @@ class MUTAG(DGLDataset):
     def process(self):
         getGraphData, _ = processData()
         graphData = getGraphData()
-        self.graphs = []
-        for graphID in graphData.keys():
-            graph, label = graphData[graphID]
-            self.graphs.append([dgl.heterograph(graph), label])
-           
-    def __getitem__(self, i):
-        return self.graphs[i]
-
-    def __len__(self):
-        return len(self.graphs)
-
-
-class MUTAGOneNtype(DGLDataset):
-    def __init__(self):
-        super().__init__(name='mutag')
-        
-    def process(self):
-        graphData = processDataOneNtype()
         self.graphs = []
         for graphID in graphData.keys():
             graph, label = graphData[graphID]
